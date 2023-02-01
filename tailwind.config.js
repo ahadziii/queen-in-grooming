51881279c759f2030a04e4aa6bbf8afff0ca5812
Edem Ahadzi <ahadzi.airdem@gmail.com>
2023-02-01 07:19:34 +0000

    configure fonts and add header component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ["Poppins", "sans-serif"],
+      },
+      backgroundImage: {
+        "hero-image": "url('/hero-image.jpg')",
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      },
+    },
   },
   plugins: [],
 }
