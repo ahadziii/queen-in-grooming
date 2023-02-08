1b4ca098625f8453c54e633493843df0d5012f64
Edem Ahadzi <ahadzi.airdem@gmail.com>
2023-02-08 21:35:30 +0000

    change background color for categort card

--- components/CategoryCards.tsx
+++ components/CategoryCards.tsx
@@ -13,14 +13,14 @@ export default function CategoryCard({
   image,
 }: CategoryProps) {
   return (
-    <div className="flex h-fit w-[320px] select-none  flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10">
+    <div className="flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#808489] p-8 md:h-[500px] md:w-[400px] md:p-10">
       <div className=" relative h-64 w-full md:h-72">
         <Image src={image} fill style={{ objectFit: "contain" }} alt={""} />
       </div>
       <div className="flex flex-1 items-center justify-between space-x-3">
-        <div className="space-y-2 text-xl text-white md:text-2xl">
-          <p>{title}</p>
-          <p>{description}</p>
+        <div>
+          <p className="space-y-2 text-xl text-white md:text-2xl">{title}</p>
+          <p className="text-white">{description}</p>
         </div>
       </div>
     </div>
